5b35f42888a513f99d7509385985b973167ac4da
Asadullah Khan <aukhan@akru.co>
2023-06-03 18:06:30 +0500

    test commit

--- server.js
+++ server.js
@@ -15,21 +15,7 @@ const { MongoClient } = require('mongodb');
 // import { MongoClient } from 'mongodb'
 
 // Connection URL
-const url = process.env.MONGODB_URI;
-const client = new MongoClient(url);
 
-// Database Name
-
-async function main() {
-  try{
-  // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  }catch(err){
-    console.log('Error connecting to server',err)
-  }
-}
-main()
 // Enable cors
 app.use(cors({
   origin: '*'
