bb135824250d46b3c9c92880153c46d6f2eb2155
Asadullah Khan <aukhan@akru.co>
2023-06-03 18:32:01 +0500

    test commit

--- utils/connection.js
+++ utils/connection.js
@@ -1,25 +1,12 @@
 const mongoose = require('mongoose');
 const { mongo } = require('../config/environment');
 function getConnection() {
-  mongoose.connect(mongo.db_url, mongo.options);
-  mongoose.connection.on('connected', () =>{
-    console.log('connection established');
-  });
+  mongoose.connect(mongo.db_url,mongo.options).then(()=>{
+    console.log("db connected")
+  }).catch(err =>{
+    console.log("Error",err)
+  })
 
-  mongoose.connection.on('error', (err) => {
-    console.log('Mongoose default connection error: ' + err);
-    reconnect()
-  });
-  mongoose.connection.on('disconnected', () => {
-    console.log('Mongoose default connection disconnected');
-   reconnect()
-  });
-
-}
-
-function reconnect(){
-  mongoose.connect(mongo.db_url, mongo.options);
-  console.log("reconnected database")
 }
 
 module.exports = { getConnection }
