4da9d1cd1a5afb304019ba5a104ce21bcc620181
Asadullah Khan <aukhan@akru.co>
2023-06-03 14:26:47 +0500

    test commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require('express');
 const cors = require("cors");
 const expressFileupload = require("express-fileupload");
-// const routes = require("./routes")
+const routes = require("./routes")
 const app = express();
 const port = process.env.PORT
 const database = require('./utils/connection');
@@ -19,7 +19,7 @@ app.use(express.json())
 // connect to db
 database.getConnection();
 
-// app.use("/api",routes)
+app.use("/api",routes)
 
 // default / route
 app.get("/", async (_, res) => {
