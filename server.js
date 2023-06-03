c63335b96012f82eb9d92cd0e9d4cd93170e4cff
Asadullah Khan <aukhan@akru.co>
2023-06-03 17:16:50 +0500

    test commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ require("dotenv").config()
 const express = require('express');
 const cors = require("cors");
 const expressFileupload = require("express-fileupload");
-const routes = require("./routes")
+// const routes = require("./routes")
 const app = express();
 const port = process.env.PORT
-const database = require('./utils/connection');
+// const database = require('./utils/connection');
 
 // Enable cors
 app.use(cors({
@@ -17,9 +17,9 @@ app.use(expressFileupload())
 app.use(express.json())
 
 // connect to db
-database.getConnection();
+// database.getConnection();
 
-app.use("/api",routes)
+// app.use("/api",routes)
 
 // default / route
 app.get("/", async (_, res) => {
@@ -33,45 +33,3 @@ app.listen(port, () => {
 });
 
 
-let portException= false;
-
-process.on('uncaughtException', (error, origin) => {
-    console.log('----- Uncaught exception -----')
-    console.log(error)
-    console.log('----- Exception origin -----')
-    console.log(origin)
-    if(error.code == "EADDRINUSE"){
-    portException= true;
-    }
- 
-
-})
-
-process.on('unhandledRejection', (reason, promise) => {
-    console.log('----- Unhandled Rejection at -----')
-    console.log(promise)
-    console.log('----- Reason -----')
-    console.log(reason)
-  
-})
-process.on('Error', (reason, promise) => {
-    console.log('----- Error Rejection at -----')
-    console.log(promise)
-    console.log('----- Reason -----')
-    console.log(reason)
-})
-process.on('error', (reason, promise) => {
-    console.log('----- error Rejection at -----')
-    console.log(promise)
-    console.log('----- Reason -----')
-    console.log(reason)
-})
-process.on('exit', (reason, promise) => {
-    console.log("Server Exit Reason::::",reason)
-    if(reason == 1){
-  
-    }else{
-      console.log("Server is shutting down .......")
-      server.close()
-    }
-})
