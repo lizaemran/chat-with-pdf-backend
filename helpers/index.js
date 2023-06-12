82c47a8d2d1b07e647f644ad06a23e6cd52714da
Asadullah Khan <aukhan@akru.co>
2023-06-12 12:14:09 +0500

    added isUserPlus

--- helpers/index.js
+++ helpers/index.js
@@ -2,25 +2,22 @@ const nodeMailer = require("nodemailer");
 require("dotenv").config();
 
 let transporter = nodeMailer.createTransport({
-  host: "smtp.privateemail.com", // your SMTP server address
-  port: 465,
-  secure: true, // true for 465, false for other ports
-  
+  host: "mail.privateemail.com", // your SMTP server address
+  port: 587,
+  secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL,
     pass: process.env.PASSWORD,
   },
 });
-
 // verify connection configuration
 transporter.verify(function (error, success) {
   if (error) {
     console.log(error);
   } else {
-    console.log("Server is ready to take our messages",success);
+    console.log("Server is ready to take our messages");
   }
 });
-
 const sendEmail = (mailOptions) => {
   return new Promise((resolve, reject) => {
     transporter.sendMail(mailOptions, (error, info) => {
